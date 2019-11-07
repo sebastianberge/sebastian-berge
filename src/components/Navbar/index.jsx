66e3b52b63a512b737447004dc6617b245eea77c
Sebastian Berge <sebastian@berge.im>
2019-11-07 18:04:35 +0100

    Added bulma and font-awesome

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,32 +1,56 @@
 import React from "react"
-import "./navbar-component.scss"
 import { Link } from "react-router-dom"
 
 // Assets
 import logo from "../../assets/images/logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+	faHome,
+	faImages,
+	faAddressCard,
+	faEnvelopeOpen
+} from "@fortawesome/free-solid-svg-icons"
 
 function Navbar() {
 	return (
-		<nav>
-			<div className='container'>
+		<React.Fragment>
+			<div class='tabs is-centered is-boxed'>
 				<ul>
-					<li>
-						<Link to='/'>
-							<img src={logo} alt='Logo' />
+					<li class='is-active'>
+						<Link>
+							<span class='icon is-small'>
+								<FontAwesomeIcon icon={faHome} />
+							</span>
+							<span>Home</span>
 						</Link>
 					</li>
 					<li>
-						<Link to='/'>home</Link>
+						<Link>
+							<span class='icon is-small'>
+								<FontAwesomeIcon icon={faImages} />
+							</span>
+							<span>Portfolio</span>
+						</Link>
 					</li>
 					<li>
-						<Link to='/'>home</Link>
+						<Link>
+							<span class='icon is-small'>
+								<FontAwesomeIcon icon={faAddressCard} />
+							</span>
+							<span>About</span>
+						</Link>
 					</li>
 					<li>
-						<Link to='/'>home</Link>
+						<Link>
+							<span class='icon is-small'>
+								<FontAwesomeIcon icon={faEnvelopeOpen} />
+							</span>
+							<span>Contact</span>
+						</Link>
 					</li>
 				</ul>
 			</div>
-		</nav>
+		</React.Fragment>
 	)
 }
 
--- src/views/MainView/index.jsx
+++ src/views/MainView/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import "./main-view.scss"
 
 class Main extends Component {
 	render() {
