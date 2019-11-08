278e70950d3e752f38dc6ee5ab40f57380947a45
Sebastian Berge <sebastian@berge.im>
2019-11-08 13:54:10 +0100

    Added image to hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,10 +1,14 @@
 import React from "react"
+import profileImg from "../../assets/images/profile.jpeg"
 
 function Hero(props) {
 	return (
 		<section className='hero is-dark is-medium'>
 			<div className='hero-body'>
 				<div className='container'>
+					<figure className='image is-128x128'>
+						<img src={profileImg} alt='Profile image' className='is-rounded' />
+					</figure>
 					<h1 className='title'>{props.title}</h1>
 					<h2 className='subtitle'>{props.subtitle}</h2>
 				</div>
